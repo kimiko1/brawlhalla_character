849db8c00dc6587d0df6517197fc558ddd505a1e
bastien <bastien.raoult1pro@gmail.com>
2023-03-17 05:14:39 +0100

    import images of character

--- models/navigation.php
+++ models/navigation.php
@@ -7,8 +7,10 @@
 
 <body>
     <nav class="navbar">
-        <form action="login.php?<?php echo htmlspecialchars(SID); ?>" method="POST">
-            <button type="submit" class="btn btn-primary "> cliquez ici pour vous connecter </button>
+    <form action="admin.php?" method="POST">
+                <?php if (@$role == "Admin") { ?>
+                    <button type="submit" class="btn btn-danger">Administration</button>
+                <?php } ?>
         </form>
         <form action="home_page.php?<?php echo htmlspecialchars(SID); ?>" method="POST">
             <button type="submit" class="btn btn-primary "> cliquez ici pour retourner a l'Accueil </button>
@@ -16,13 +18,8 @@
         <form action="logout.php?<?php echo htmlspecialchars(SID); ?>" method="POST">
             <button action="" type="submit" class="btn btn-primary "> cliquez ici pour vous déconnectez </button>
         </form>
-
-        <form action="admin.php?" method="POST">
-            <?php if (isset($role)): ?>
-                <?php if ($role == "Admin") { ?>
-                    <button type="submit" class="btn btn-danger">Administration</button>
-                <?php } ?>
-            <? endif; ?>
+        <form action="login.php?<?php echo htmlspecialchars(SID); ?>" method="POST">
+            <button type="submit" class="btn btn-primary "> cliquez ici pour vous connecter </button>
         </form>
     </nav>
 </body>
